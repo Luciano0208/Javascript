da09b43ed9230ffec64b1a8b512af8201cf24b42
Luciano0208 <ks2575987@gmail.com>
2023-07-19 07:21:12 -0300

    alterei o exercicio

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -5,6 +5,25 @@ switch(diaSem) {
     case 0:
         console.log('domingo')
         break
-        case 1:
-            console.log('segunda')
+    case 1:
+        console.log('segunda')
+        break
+    case 2: 
+        console.log('terça')
+        break
+    case 3:
+        console.log('quarta')
+        break
+    case 4:
+        console.log('quinta')
+        break
+    case 5:
+        console.log('sexta')
+        break
+    case 6:
+        console.log('sábado')
+        break
+        default:
+            console.log('[Erro] Dia inválido!')
+            break
 }
