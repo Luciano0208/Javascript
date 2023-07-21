6b21cef68ae3560a20619c95dfd8061617a067bf
Luciano0208 <ks2575987@gmail.com>
2023-07-21 16:18:28 -0300

    alterei o codigo

--- aula12ex/script.js
+++ aula12ex/script.js
@@ -1,1 +1,7 @@
-alert('Olá, Mundo')
+function carregar() {
+var msg = document.getElementById('msg')
+var img = document.getElementById('foto')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+}
