d7cec422cf9de9b3934c3f33786df6471f92138f
Luciano0208 <ks2575987@gmail.com>
2023-07-24 08:02:32 -0300

    acabei exercicio

--- aula12ex/script.js
+++ aula12ex/script.js
@@ -1,7 +1,20 @@
 function carregar() {
 var msg = document.getElementById('msg')
-var img = document.getElementById('foto')
+var img = document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) {
+    //BOM DIA
+    img.src = 'manhanovo-250.png'
+    document.body.style.background = '#e2cd9f'
+
+} else if (hora >= 12 && hora < 18) {
+    //BOA TARDE
+    foto.src = 'tarde-novo.png'
+    document.body.style.background = '#b9846f'
+} else {
+    //boa noite
+    img.src = 'noite-novo.png'
+}
 }
